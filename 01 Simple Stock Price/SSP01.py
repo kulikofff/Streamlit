05f79e6a964c44576642c38dcbabcc1fd11c5ddf
Andrey Kulikofff <kulikofff@outlook.com>
2022-05-26 19:12:26 +0300

    commit

--- 01 Simple Stock Price/SSP01.py
+++ 01 Simple Stock Price/SSP01.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import streamlit as st
 
 st.write("""
-##### Simple Stock Price App
+# Simple Stock Price App
 Shown are the stock **closing price** and ***volume*** of Google!
 """)
 
